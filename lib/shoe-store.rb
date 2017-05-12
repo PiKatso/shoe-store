25beb60d721c25fa1a86916c169dedb3e35a544e
Kat Gifford <katgiff2007@gmail.com>
2017-05-12 09:37:27 -0700

    add validations for characters max and capitalize

--- lib/shoe-store.rb
+++ lib/shoe-store.rb
@@ -1,12 +1,22 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many :stores
+  validates :name, length: { maximum: 100 }
+  before_save :capitalize_title
 end
 
 class Store < ActiveRecord::Base
   has_and_belongs_to_many :brands
+  validates :name, length: { maximum: 100 }
+  before_save :capitalize_title
 end
 
 class BrandsStores < ActiveRecord::Base
   belongs_to :brand
   belongs_to :store
+end
+
+private
+
+def capitalize_title
+  self.name = name.split.map(&:capitalize).join(" ")
 end
