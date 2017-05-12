f42dea90539206937e1736251015e47537c4bbaf
Kat Gifford <katgiff2007@gmail.com>
2017-05-12 10:10:14 -0700

    add validation testing

--- lib/shoe-store.rb
+++ lib/shoe-store.rb
@@ -1,27 +1,36 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many :stores
 
-  validates :price, numericality: true
+  validates :price, presence: true
+  validates :price, numericality: { only_integer: true }
+  validates :name, presence: true, length: { maximum: 100 }
   validates :name, uniqueness: { case_sensitive: false }
-  validates :name, length: { maximum: 100 }
+
   before_save :capitalize_title
+
+  private
+
+  def capitalize_title
+    self.name = name.split.map(&:capitalize).join(" ")
+  end
 end
 
 class Store < ActiveRecord::Base
   has_and_belongs_to_many :brands
 
+  validates :name, presence: true, length: { maximum: 100 }
   validates :name, uniqueness: { case_sensitive: false }
-  validates :name, length: { maximum: 100 }
+
   before_save :capitalize_title
+
+  private
+
+  def capitalize_title
+    self.name = name.split.map(&:capitalize).join(" ")
+  end
 end
 
 class BrandsStores < ActiveRecord::Base
   belongs_to :brand
   belongs_to :store
-end
-
-private
-
-def capitalize_title
-  self.name = name.split.map(&:capitalize).join(" ")
 end
--- spec/shoe_store_spec.rb
+++ spec/shoe_store_spec.rb
@@ -2,6 +2,27 @@ require "spec_helper"
 
 describe Brand do
   it { should have_and_belong_to_many(:stores) }
+
+  it("validates presence of price") do
+    test_brand = Brand.create({:name => "kat", :price => ""})
+    expect(test_brand.save()).to(eq(false))
+  end
+
+  it("validates numericality of price") do
+    test_brand = Brand.create({:name => "kat", :price => "kat50.00"})
+    expect(test_brand.save()).to(eq(false))
+  end
+
+  it("validates presence of name") do
+    test_brand = Brand.create({:name => "", :price => "50.00"})
+    expect(test_brand.save()).to(eq(false))
+  end
+
+  it("ensures the length of description is at most 100 characters") do
+    test_brand = Brand.create({:name => "k".*(101)})
+    expect(test_brand.save()).to(eq(false))
+  end
+
 end
 
 describe Store do
