7ae10580c65539408854ce1836522a0ec1388f24
Kat Gifford <katgiff2007@gmail.com>
2017-05-12 11:18:51 -0700

    start routing for brands/stores pages

--- app.rb
+++ app.rb
@@ -9,3 +9,15 @@ also_reload('lib/**/*.rb')
 get('/') do
   erb(:index)
 end
+
+# Store routing
+get('/stores') do
+  @store_message = Store.all.length > 0 ? "Select a store to review brands carries" : "No stores yet, add a store."
+  erb :store
+end
+
+# Brand routing
+get('/brands') do
+  @brand_message = Brand.all.length > 0 ? "Select a brand to review prices and locations for purchase" : "Add a brand below"
+  erb :brand
+end
